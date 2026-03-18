56eac9cc3e43e9f824ca4229d57cc485170c88bb
Yoki Valianda <50901028+yokivalianda@users.noreply.github.com>
2026-03-18 11:18:25 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+// MarketPro Service Worker v1.0
+const CACHE_NAME = 'marketpro-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
